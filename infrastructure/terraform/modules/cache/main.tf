ef6c04a00b5316c25f465da4cc60c85a07fd437f
Avi-Bendetsky <146357256+Avi-Bendetsky@users.noreply.github.com>
2026-01-27 01:03:18 +1100

    commit 767fi5

--- infrastructure/terraform/modules/cache/main.tf
+++ infrastructure/terraform/modules/cache/main.tf
@@ -18,23 +18,3 @@ resource "azurerm_redis_cache" "main" {
 
   tags = var.tags
 }
-# Cache Module - Azure Cache for Redis
-
-resource "azurerm_redis_cache" "main" {
-  name                = "redis-${var.project_name}-${var.environment}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  capacity            = var.capacity
-  family              = var.family
-  sku_name            = var.sku_name
-  enable_non_ssl_port = false
-  minimum_tls_version = "1.2"
-
-  redis_configuration {
-    maxmemory_reserved = 10
-    maxmemory_delta    = 10
-    maxmemory_policy   = "allkeys-lru"
-  }
-
-  tags = var.tags
-}
